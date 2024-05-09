c75047a9733822427e9e7188157b446ea5c2c324
jonathanplacatka <49293922+jonathanplacatka@users.noreply.github.com>
2024-05-09 15:35:11 -0500

    User input logic (#1)
    * add logic for single word input
    
    * move code into Game component
    
    * add components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 "use client"
 
-import { useState } from "react";
-import HighlightText from "./HighlightText";
+import Game from "../components/Game";
 
 export default function Home() {
-  const sentence = "one two three four five six seven"
-
-  const [input, setInput] = useState('');
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value)
-  } 
-
   return (
     <main>
-      <HighlightText value={sentence} compareString={input} />
-      <input autoFocus onChange={onChange}/>
+      <Game/>
     </main>
   );
 }
