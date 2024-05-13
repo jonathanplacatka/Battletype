6cbadbf795e6549bec19c9fd549da7245f499b4c
Jonathan Placatka <jplacatka@live.ca>
2024-05-12 19:44:32 -0500

    remove old imports

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client"
 
-import { io } from "socket.io-client";
-import ButtonSocketConnection from "./components/ButtonSocketConnection";
 import Game from "./components/Game";
-import GameWindow from "./components/GameWindow";
 
 export default function Home() {
   return (
