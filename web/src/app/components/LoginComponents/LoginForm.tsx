878e863fb312fc7439214f992ebf5f9917cb101f
Ying Liang <93963545+YingLiang2@users.noreply.github.com>
2024-07-17 18:57:38 -0500

    Added in a simple login form to the main page so when the user enters our website, they can either play as a guest or enter their own unique name. (#28)
    The logic for their own username has not been implemented.

---
+++ web/src/app/components/LoginComponents/LoginForm.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import LogoV1 from "image/LogoV1.svg"
+import Image from "next/image";
+import { useRouter } from 'next/navigation'
+
+
+export default function LoginForm() {
+
+    const router = useRouter()
+
+    return (
+        <div className='flex flex-col justify-center items-center my-10'>
+            <div className="relative inline-flex flex-col justify-center items-center rounded-lg bg-[#242424] p-6 mt-8">
+                <div className="flex flex-col mb-3">
+                    <Image src={LogoV1} alt="logo" width={550} height={150} className=""/> 
+                </div>
+
+                <div className="flex flex-col justify-center items-center w-full my-14" >
+                    <input className="p-2 rounded border border-gray-300 bg-white"
+                    placeholder="Enter a username">
+                    </input>
+                </div>
+            
+                <div className="">
+                    <div className='flex flex-col p-3 mx-4'>
+                        <button className ='btntext bg-[#2C2C2C] hover:bg-blue-700 text-white py-2 px-3 mx-5 my-4 rounded-lg' onClick={() => {router.push("1234")}}>Enter</button>
+                        <button className ='btntext bg-[#2C2C2C] hover:bg-blue-700 text-white py-2 px-3 mx-5 my-4 rounded-lg'>Play as Guest</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
