0bb9a1a1f1c6b509bd9fb38b7c23833f5a7fcb8d
Ying Liang <93963545+YingLiang2@users.noreply.github.com>
2024-07-28 18:02:10 -0500

    Added small fixes in (#40)
    * add indicator for current player in game screen e.g player1234 (You)
    * when leaving the a lobby, it redirects you to the multiplayer page

--- web/src/app/components/Scoreboard.tsx
+++ web/src/app/components/Scoreboard.tsx
@@ -5,15 +5,16 @@ import playerColors from '@/scripts/PlayerColors';
 
 interface ScoreboardProps {
     players: PlayerState
+    playerID: string;
     numWords: number;
 }
 
-export default function ScoreboardProps({players, numWords}: ScoreboardProps) {
+export default function ScoreboardProps({players, playerID, numWords}: ScoreboardProps) {
   return (
     <>
         {Object.entries(players).map(([id, {username, score, WPM, place}], index) => (
             <>
-                <p>{username}</p>
+                <p>{id === playerID ? `${username} (you)` : username }</p>
                 <div className="flex flex-row">
                     <ProgressBar score={score} numWords={numWords} color={playerColors[index]} />
                     <p className="-mt-1 ml-2">{0} wpm</p>
