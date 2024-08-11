87411c04d1ac83e9c27ca5260460bccb53209ae5
Jonathan Placatka <jplacatka@live.ca>
2024-08-10 20:54:59 -0500

    cleanup margin css

--- web/src/app/components/LobbyPlayerList.tsx
+++ web/src/app/components/LobbyPlayerList.tsx
@@ -12,7 +12,7 @@ export default function LobbyPlayerList({players, playerID} : LobbyPlayerListPro
             <ul>
                 {Object.entries(players).map(([id, {username, host}], index) => (
                     <div key={id + username}>
-                        <li className="flex mx-2 my-2">
+                        <li className="flex m-2">
                             <span className="text-white">{id === playerID ? `${username} (you)` : username }</span>
                             {host &&  <svg className="ml-2" xmlns="http://www.w3.org/2000/svg" width="1em" height="1.5em" viewBox="0 0 24 24"><path fill="#FFB743" d="M5 16L3 5l5.5 5L12 4l3.5 6L21 5l-2 11zm14 3c0 .6-.4 1-1 1H6c-.6 0-1-.4-1-1v-1h14z"/></svg>}
                         </li>  
