b8a5ba2a5cf077c8d62c9d2df91bbd37d27c3bb5
Ying <yingliang3@hotmail.com>
2024-08-11 22:29:17 -0500

    Finalized most of the UI refactor.

--- web/src/app/components/GamemodeButton.tsx
+++ web/src/app/components/GamemodeButton.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react";
+
 
 type IconType = React.FC<React.SVGProps<SVGSVGElement>> | (() => JSX.Element);
 
@@ -8,20 +10,21 @@ interface GamemodeButtonProps {
     title: string
     description: string
     onClickEvent: () => void
+    optionalClass?: string
 }
 
-export default function GamemodeButton({icon, title, description, onClickEvent} : GamemodeButtonProps) {
-
+export default function GamemodeButton({icon, title, description, onClickEvent, optionalClass} : GamemodeButtonProps) {
 
     return (
-        <button className ='flex flex-col mb-4 p-4 bg-[#EBEBEB] hover:bg-blue-700 text-black rounded-lg' onClick={onClickEvent}>
+        <button className ={`flex flex-col my-3 p-4 bg-[#EBEBEB] hover:bg-blue-700 text-black max-h-[92px] rounded-lg ${optionalClass}`} onClick={onClickEvent}>
                             
             <div className='mb-2 flex items-center'>
                 {icon}
                 <span className='ml-4 font-bold'>{title}</span>
             </div>
             
-            <span className=' text-sm'>{description}</span>
+            <span className='text-sm overflow-hidden'>{description}</span>
+            
         </button>
     );
 
