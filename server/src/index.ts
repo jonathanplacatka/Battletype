ef31db0d3dcfc3d20eb3002b658bda1ccb54d17d
jonathanplacatka <49293922+jonathanplacatka@users.noreply.github.com>
2024-08-14 19:34:30 -0500

    Add environment variables for game server (#59)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,20 @@
 import GameServer from "./server";
 
 function main() {
-  let server = new GameServer(4000, "http://localhost:3000") //should setup env files for this
-  server.start();
+
+	if(process.env.NODE_ENV !== 'prod') {
+		require('dotenv').config();
+	}
+
+	const port = process.env.PORT;
+	const origin = process.env.ORIGIN;
+
+	if(port && origin) {
+		let server = new GameServer(parseInt(port), origin) 
+    	server.start()
+	} else {
+    	console.log("environment variables not found")
+  	}
 }
 
 main();
