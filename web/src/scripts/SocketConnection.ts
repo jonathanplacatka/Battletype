8c71ae26660e05fa5111c0f943c3a13bfc20a9ef
jonathanplacatka <49293922+jonathanplacatka@users.noreply.github.com>
2024-08-14 20:10:28 -0500

    add env variable (#60)

--- web/src/scripts/SocketConnection.ts
+++ web/src/scripts/SocketConnection.ts
@@ -1,9 +1,9 @@
 import io, { Socket } from 'socket.io-client';
 
-const SERVER_URL = 'http://localhost:4000'; 
+const url = process.env.NEXT_PUBLIC_GAME_SERVER_URL ?? "http://localhost:4000";
 
-const socket : Socket = io(SERVER_URL, {
+const socket : Socket = io(url, {
     autoConnect: false
-})
+});
 
 export default socket;
