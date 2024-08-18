4aa086e84f765192e0e4003a89c26f80a7ef4890
Ying <yingliang3@hotmail.com>
2024-08-18 17:45:21 -0500

    Removed code from header

--- web/src/app/components/PageLayout/Header.tsx
+++ web/src/app/components/PageLayout/Header.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <div className="flex px-6 py-4 border-b border-white">
-            <nav className="flex justify-between w-full">
+            <nav>
                 <Link href="/">
                     <Image className="w-auto h-auto max-w-[300px] max-h-[70px]" src={logov2} alt="logo"/> 
                 </Link>
